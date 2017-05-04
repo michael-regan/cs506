0aeb75565ef2037cd805388b49adf78589e57441
michael-regan <michael-regan@users.noreply.github.com>
2017-05-04 01:18:40 -0600

    methodize compiledDict

--- makeSentVectors.py
+++ makeSentVectors.py
@@ -1,32 +1,9 @@
 import pickle
 import numpy as np
+from compileCorpus import compiledDict
 
 # simple experiments with svd word vectors for sentiment analysis
 # svd done with svd.py
-
-neg_path = 'polarityData/rt-polaritydata/negative.txt'
-
-pos_path = 'polarityData/rt-polaritydata/positive.txt'
-
-negDict={}
-posDict={}
-
-
-cnt=0
-with open(neg_path, 'r') as f:
-
-	for line in f:
-		negDict['neg'+str(cnt)]=line
-		cnt+=1
-
-
-with open(pos_path, 'r') as g:
-
-	for line in g:
-		posDict['pos'+str(cnt)]=line
-		cnt+=1
-
-compiledDict = {**negDict, **posDict}
 
 # length of compiledDict is 10662
 # print(len(compiledDict)) 
