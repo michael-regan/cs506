e9446eed73e1ecc45cb7b91cd9bf79a7c9c78917
michael-regan <michael-regan@users.noreply.github.com>
2017-05-04 01:20:02 -0600

    Add compiledDict as method

--- corpusStats.py
+++ corpusStats.py
@@ -4,37 +4,13 @@ import pandas as pd
 
 from collections import Counter
 from pandas import Series
+from compileCorpus import compiledDict
 
 
 # preparing basic stats on sentiment analysis corpus
 
 # sentiment data from: https://github.com/shekhargulati/sentiment-analysis-python/tree/master/polarity-data
 # small corpus of 21425 unique tokens
-
-
-neg_path = 'polarityData/rt-polaritydata/negative.txt'
-
-pos_path = 'polarityData/rt-polaritydata/positive.txt'
-
-negDict={}
-posDict={}
-
-
-cnt=0
-with open(neg_path, 'r') as f:
-
-	for line in f:
-		negDict['neg'+str(cnt)]=line
-		cnt+=1
-
-
-with open(pos_path, 'r') as g:
-
-	for line in g:
-		posDict['pos'+str(cnt)]=line
-		cnt+=1
-
-compiledDict = {**negDict, **posDict}
 
 # dividing into sentences and tokens for distribution (plot)
 sentences = []
