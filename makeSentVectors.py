ea936701858297eb1aeae0d7a0a6e986f6fef37c
michael-regan <michael-regan@users.noreply.github.com>
2017-05-05 22:47:41 -0600

    Import dicts from matchWords2Vectors

--- makeSentVectors.py
+++ makeSentVectors.py
@@ -1,69 +1,90 @@
 import pickle
 import numpy as np
 from compileCorpus import compiledDict
+from matchWords2Vectors import dict_25d, dict_50d, dict_100d
 
 # simple experiments with svd word vectors for sentiment analysis
 # svd done with svd.py
 
-# length of compiledDict is 10662
-# print(len(compiledDict)) 
-# print()
 
+vectors_25d = []
+vectors_50d = []
+vectors_100d = []
 
-data = 'data/dict_100d'
+dimensions = [25, 50, 100]
 
-with open(data, 'rb') as h:
-	word_with_vectors = pickle.load(h)
+for dimension in dimensions:
 
-
-compiled_sentence_vectors = {}
-
-errorCnt=0
-for k, v in compiledDict.items():
-	sentence = v.split()
-	length = len(sentence)
-
-	key_error = False
-
-	m = np.zeros(100)
-
-	for word in sentence:
-
-		try:
-			word_vec = word_with_vectors[word]
-			m+=word_vec
-
-		except KeyError:
-			errorCnt+=1
-			key_error = True
-			break
-
-	# sentence vectors = summing all vectors from sentence and averaging --> the vector mash
-	if key_error == False:
-		norm_m = m/length
-		compiled_sentence_vectors[k] = norm_m
-
-
-# length: 10499; Key errors: 163
-# print(len(compiled_sentence_vectors))
-# print("Key errors", errorCnt)
-
-labels = []
-vectors = []
-vector_ids = []
-
-for k, v in compiled_sentence_vectors.items():
-
-	if 'neg' in k:
-		labels.append(0)
+	#data = 'data/dict_100d'
+	if dimension == 25:
+		data = dict_25d
+	elif dimension == 50:
+		data = dict_50d
 	else:
-		labels.append(1)
+		data = dict_100d
 
-	vectors.append(v)
+	# with open(data, 'rb') as h:
+	# 	word_with_vectors = pickle.load(h)
 
-	vector_ids.append(k)
+	word_with_vectors = data
+	
+	compiled_sentence_vectors = {}
+
+	errorCnt=0
+	for k, v in compiledDict.items():
+		sentence = v.split()
+		length = len(sentence)
+
+		key_error = False
+
+		m = np.zeros(dimension)
+
+		for word in sentence:
+
+			try:
+				word_vec = word_with_vectors[word]
+				m+=word_vec
+
+			except KeyError:
+				errorCnt+=1
+				key_error = True
+				break
+
+		# sentence vectors = summing all vectors from sentence and averaging --> the vector mash
+		if key_error == False:
+			norm_m = m/length
+			compiled_sentence_vectors[k] = norm_m
+
+	# Key errors: 163
+	print("Key errors", errorCnt)
+
+	#labels and vectors remain the same for all dimensionalities; vectors will change
+	labels = []
+	vectors = []
+	vector_ids = []
+
+	for k, v in compiled_sentence_vectors.items():
+
+		if 'neg' in k:
+			labels.append(0)
+		else:
+			labels.append(1)
+
+		vectors.append(v)
+
+		vector_ids.append(k)
 
 
-pickle.dump(labels, open('data/3_labels_100d', 'wb'), protocol=4)
-pickle.dump(vectors, open('data/3_vectors_100d', 'wb'), protocol=4)
-pickle.dump(vector_ids, open('data/3_vectorIDs_100d', 'wb'), protocol=4)
+	if dimension == 25:
+		vectors_25d = vectors
+
+	elif dimension == 50:
+		vectors_50d = vectors
+
+	else: 
+		vectors_100d = vectors
+
+
+	# pickle.dump(labels, open('data/3_labels_100d', 'wb'), protocol=4)
+	# pickle.dump(vectors, open('data/3_vectors_100d', 'wb'), protocol=4)
+	# pickle.dump(vector_ids, open('data/3_vectorIDs_100d', 'wb'), protocol=4)
